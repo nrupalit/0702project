f17b388a7bcdf45d2620df85abc1b60aa74aa3cf
Nrupalit <nrupali12345@gmail.com>
2020-02-10 10:21:59 +1100

    commit

--- clientsideproject/src/Components/RoutePages.js
+++ clientsideproject/src/Components/RoutePages.js
@@ -3,7 +3,6 @@ import {  Switch , Link , BrowserRouter as Router, Route } from 'react-router-do
 import App from '../App';
 import Index from './Index';
 import NotFoundPage from './NotFoundPage';
-import AntDTry from './AntDTry';
 
 class RoutePages extends Component {
     render () {
@@ -12,7 +11,6 @@ class RoutePages extends Component {
                 <Switch>
                     <Route path="/login" component = { App }/>
                     <Route exact path="/" component = { Index }/>
-                    <Route path="/try" component = { AntDTry }/>
                     <Route component={NotFoundPage} />
                 </Switch>
                 <ul>
